6cb86b36d135616312fb88e6028c24b6e9b34b8b
Nils <55735148+NilsMillot@users.noreply.github.com>
2021-07-19 22:57:09 +0200

    fix google auth and add discord

--- oauth-client/index.php
+++ oauth-client/index.php
@@ -6,17 +6,11 @@ const CLIENT_GTID = "cd08eb1a4191742c3488";
 const CLIENT_SECRET = "cd989e9a4b572963e23fe39dc14c22bbceda0e60";
 const CLIENT_FBSECRET = "cfd0d0117ba19c789c711b1f0afaf3c4";
 const CLIENT_GTSECRET = "72c05c3dd00e1a8cbba435a330228a617e008eb9";
-const CLIENT_GOOGLEID = "1086263128848-hd7hr0j46vmfled59k7pib2l7ueicrat.apps.googleusercontent.com";
-const CLIENT_GOOGLESECRET = "JOCm3dScQn5x4xlU_Ybo99Oo";
 const CLIENT_DISCORD_ID = "866623043559751720";
 const CLIENT_DISCORD_SECRET = "olGHze-0iWhXltjLbpnqmoPVtgRjCrx_";
+const CLIENT_GOOGLEID= "666723567104-k4aguknbo73rlr7b12gnnin4791ssn5t.apps.googleusercontent.com";
+const CLIENT_GOOGLESECRET = "b9BzjhKAYYmHnqA18L2RI11U";
 
-
-// CLE Nils :
-// const CLIENT_FBID = "3648086378647793";
-//const CLIENT_GOOGLEID= "666723567104-k4aguknbo73rlr7b12gnnin4791ssn5t.apps.googleusercontent.com";
-// const CLIENT_FBSECRET = "1b5d764e7a527c2b816259f575a59942";
-//const CLIENT_GOOGLESECRET = "b9BzjhKAYYmHnqA18L2RI11U";
 
 const STATE = "fdzefzefze";
 
@@ -117,62 +111,76 @@ function handleGtSuccess()
 function handleGoogleSuccess()
 {
     ["code" => $code] = $_GET;
-    // if ($state !== STATE) {
-    //     throw new RuntimeException("{$state} : invalid state");
-    // }
-    // Alternative
-    // $url = "https://www.googleapis.com/oauth2/v4/token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_GOOGLEID . "&client_secret=" . CLIENT_GOOGLESECRET . "&redirect_uri=http://localhost:8082/googleauth-success";
-    $url = "https://oauth2.googleapis.com/token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_GOOGLEID . "&client_secret=" . CLIENT_GOOGLESECRET . "&redirect_uri=http://localhost:8082/googleauth-success";
-    $result = file_get_contents($url);
-    $resultDecoded = json_decode($result, true);
-    ["access_token"=> $token] = $resultDecoded;
-    $userUrl = "https://openidconnect.googleapis.com/v1/userinfo?fields=name,email";
-    $curl = curl_init($userUrl);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0.2 Safari/602.3.12");
-    curl_setopt($curl, CURLOPT_HTTPHEADER,["Authorization: Bearer {$token}"]);
-    curl_setopt($curl,CURLOPT_HEADER,0);
-    $result = curl_exec($curl);
-    // echo $result;
+    $postdata = http_build_query(
+        array(
+            'code' => $code,
+            'client_id' => CLIENT_GOOGLEID,
+            'client_secret' => CLIENT_GOOGLESECRET,
+            'redirect_uri' => "http://localhost:8082/googleauth-success",
+            'grant_type' => 'authorization_code',
+        )
+    );
+    $opts = array('http' =>
+        array(
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+    $context = stream_context_create($opts);
+    $result = file_get_contents('https://oauth2.googleapis.com/token', false, $context);
+    echo $result;
+    $resultDecoded = json_decode($result);
 
-    // ["state" => $state, "code" => $code] = $_GET;
-    // if ($state !== STATE) {
-    //     throw new RuntimeException("{$state} : invalid state");
-    // }
-    // $url = "https://oauth2.googleapis.com/token?grant_type=authorization_code&code={$code}&client_id=" . CLIENT_GOOGLEID . "&client_secret=" . CLIENT_GOOGLESECRET . "&redirect_uri=https://localhost:8082/googleauth-success";
-    // $result = file_get_contents($url);
-    // var_dump($result);
-    // $resultDecoded = json_decode($result, true);
-    // ["access_token"=> $token] = $resultDecoded;
-    // $userUrl = "https://openidconnect.googleapis.com/v1/userinfo?fields=name,email";
-    // $context = stream_context_create([
-    //     'http' => [
-    //         'header' => 'Authorization: Bearer ' . $token
-    //     ]
-    // ]);
-    // echo file_get_contents($userUrl, false, $context);
+    $token = $resultDecoded->{'access_token'};
+    $userUrl = "https://openidconnect.googleapis.com/v1/userinfo?fields=name,email";
+    $context = stream_context_create([
+        'http' => [
+            'header' => 'Authorization: Bearer ' . $token
+        ]
+    ]);
+    echo file_get_contents($userUrl, false, $context);
 }
 
 function handleDiscordSuccess()
 {
     ["code" => $code] = $_GET;
-    echo $code;
+
+    $postdata = http_build_query(
+        array(
+            'code' => $code,
+            'client_id' => CLIENT_DISCORD_ID,
+            'client_secret' => CLIENT_DISCORD_SECRET,
+            'redirect_uri' => "http://localhost:8082/discordauth-success",
+            'grant_type' => 'authorization_code',
+        )
+    );
+    $opts = array('http' =>
+        array(
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+    $context = stream_context_create($opts);
+    $result = file_get_contents('discord.com/api/oauth2/authorize', false, $context);
+    echo $result;
     
     // if ($state !== STATE) throw new RuntimeException("{$state} : invalid state");
-    $url = "https://discord.com/api/oauth2/authorize?grant_type=authorization_code&code=${code}&client_id=" . CLIENT_DISCORD_ID . "&client_secret=" . CLIENT_DISCORD_SECRET . "&redirect_uri=discordauth-success";
-    // "&client_secret=" . CLIENT_DISCORD_SECRET . "&redirect_uri=discordauth-success";
-    $result = file_get_contents($url);
+    // $url = "https://discord.com/api/oauth2/authorize?grant_type=authorization_code&code=${code}&client_id=" . CLIENT_DISCORD_ID . "&client_secret=" . CLIENT_DISCORD_SECRET . "&redirect_uri=discordauth-success";
+    // $result = file_get_contents($url);
 
-    // $string = explode("&", $result, 2)[0];
-    // $token = explode("=", $string)[1];
-    // $userUrl = "https://api.github.com/user";
+    // $result = file_get_contents($url);
+    // $resultDecoded = json_decode($result, true);
+    // ["access_token"=> $token] = $resultDecoded;
+    // $userUrl = "https://discord.com/oauth2/@me?scope=email";
     // $curl = curl_init($userUrl);
     // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     // curl_setopt($curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0.2 Safari/602.3.12");
     // curl_setopt($curl, CURLOPT_HTTPHEADER,["Authorization: Bearer {$token}"]);
     // curl_setopt($curl,CURLOPT_HEADER,0);
     // $result = curl_exec($curl);
-    echo $result;
+    // echo $result;
     //   echo file_get_contents($userUrl,false ,$context);
 }
 
